2fddcdfd04917c73c64a9e85312f31d56fbaa4e3
Rets Dima <d.rets@timeweb.ru>
2021-01-31 20:51:59 +0300

    add resize utility

--- src/scripts/pages/Playground/PlayGroundItem.js
+++ src/scripts/pages/Playground/PlayGroundItem.js
@@ -7,7 +7,7 @@ const PlaygroundItem = () => {
 
   return(
     <div className="mb-4 sh-2 p-5 pos-rel">
-      <textarea onInput={e=>setClassList(e.target.value)} placeholder="Element class list (with space)" className="wd-full mb-7 p-3 out-n rds-4 bdr bdr-wd-2 bdr-secondary"></textarea>
+      <textarea onInput={e=>setClassList(e.target.value)} placeholder="Element class list (with space)" className="rsz-y wd-full mb-7 p-3 out-n rds-4 bdr bdr-wd-2 bdr-secondary"></textarea>
       <div suppressContentEditableWarning="true" className={classList + ' out-n'} contentEditable>Element some text (editable)</div>
     </div>
   )
